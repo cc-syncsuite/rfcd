c6e3fa9ec246a1c3db03243d36fe55fa429e0761
Alexander Surma <surma@zib.de>
2010-03-22 16:15:12 +0100

    Configurable protocol delimiters/separators, req struct

--- rfcd.go
+++ rfcd.go
@@ -7,6 +7,11 @@ import (
 	"os"
 	"json"
 	"fmt"
+	"strconv"
+	"net"
+	"time"
+	"bufio"
+	"strings"
 )
 
 const (
@@ -17,14 +22,48 @@ var (
 	globalConfig rfcdConfig
 )
 
-
 type rfcdConfig struct {
-	BindAddr string
-	Port int
-	Debug int
+	BindAddr  string
+	Port      int
+	Verbosity int
+	Delimiter string
+	Separator string
 }
 
-func parseCmdLine() (string) {
+func (c *rfcdConfig) GetSeparatorChar() byte { return c.Separator[0] }
+
+func (c *rfcdConfig) GetDelimiterChar() byte { return c.Delimiter[0] }
+
+type Request struct {
+	con net.Conn
+}
+
+func NewRequest(con net.Conn) (r Request) {
+	r.con = con
+	return
+}
+
+func (req *Request) GetWriter() io.Writer { return req.con }
+
+func (req *Request) GetReader() io.Reader { return req.con }
+
+func (req *Request) GetRemoteAddr() string { return req.con.RemoteAddr().String() }
+
+func panicOnError(msg string, e os.Error) {
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", e)
+		panic(msg)
+	}
+}
+
+func debug(level int, msg string, a ...interface{}) {
+	if globalConfig.Verbosity >= level {
+		newfmt := fmt.Sprintf("DEBUG(%d): [%s] ", level, time.LocalTime())
+		fmt.Printf(newfmt+msg+"\n", a)
+	}
+}
+
+func parseCmdLine() string {
 	file := flag.String("c", DEFAULT_CONF, "Path to configuration file")
 	flag.Parse()
 
@@ -37,7 +76,7 @@ func readConfig(r io.Reader) (rfcdConfig, os.Error) {
 	if error == nil {
 		ok, errTok := json.Unmarshal(string(bRead), &config)
 		if !ok {
-			error = os.NewError("Offending Token in config file: "+errTok)
+			error = os.NewError("Offending Token in config file: " + errTok)
 		}
 	}
 	return config, error
@@ -51,16 +90,55 @@ func readConfigFile(file string) (config rfcdConfig, error os.Error) {
 	return
 }
 
-func panicOnError(msg string, e os.Error) {
-	if e != nil {
-		fmt.Fprintf(os.Stderr, "Error: %s\n", e)
-		panic(msg)
+
+func fillRequestChannel(listener net.Listener, reqChannel chan Request) {
+	for {
+		con, e := listener.Accept()
+		if e == nil {
+			req := NewRequest(con)
+			debug(1, "%s connected", req.GetRemoteAddr())
+			reqChannel <- req
+		}
+	}
+}
+
+func setupServer(addr string) (chan Request, os.Error) {
+	tcpAddr, e := net.ResolveTCPAddr(addr)
+	if e == nil {
+		listener, e := net.ListenTCP("tcp4", tcpAddr)
+		debug(2, "Created listener. Error: %s", e)
+		if e == nil {
+			reqChannel := make(chan Request, 5)
+			go fillRequestChannel(listener, reqChannel)
+			return reqChannel, e
+		}
+		return nil, e
+	}
+	return nil, e
+}
+
+func clientHandler(req Request) {
+	br := bufio.NewReader(req.GetReader())
+	for entity, e := br.ReadString(globalConfig.GetDelimiterChar()); e == nil; entity, e = br.ReadString(globalConfig.GetDelimiterChar()) {
+		debug(1, "%s: Received \"%s\"", req.GetRemoteAddr(), entity)
+		elems := strings.Split(entity[0:len(entity)-1], globalConfig.Separator, 0)
+		for k, s := range elems {
+			debug(2, "%s: Tokenlist: %d = \"%s\"", req.GetRemoteAddr(), k, s)
+		}
 	}
 }
 
 func main() {
 	configFile := parseCmdLine()
 
-	globalConfig, e := readConfigFile(configFile)
+	config, e := readConfigFile(configFile)
+	globalConfig = config
+	debug(2, "Binding address: %s:%d", globalConfig.BindAddr, globalConfig.Port)
 	panicOnError("Reading configuration failed", e)
+
+	reqChannel, e := setupServer(globalConfig.BindAddr + ":" + strconv.Itoa(globalConfig.Port))
+	panicOnError("Opening server failed", e)
+	for req := range reqChannel {
+		go clientHandler(req)
+	}
 }
